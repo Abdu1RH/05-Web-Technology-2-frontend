aa1e77a718693ec10d62872153e4212ddee0996e
Abdu1RH <abdul.rh2403@gmail.com>
2022-11-21 11:24:07 +0100

    THIRD COMMIT

--- main.js
+++ main.js
@@ -16,9 +16,11 @@ function renderSpots(spots) {
         console.log(spot);
         const li = document.createElement('li');
         li.innerHTML = `
+            <p class="spot_id">${spot.spot_id}</p>
             <p class="location_name">${spot.location_name}</p>
-            
             <p class="address">${spot.address}</p>
+            <p class="longtitude">${spot.longitude}</p> 
+            <p class="latitude">${spot.latitude}</p>
         `;
         ul.appendChild(li);
     }
