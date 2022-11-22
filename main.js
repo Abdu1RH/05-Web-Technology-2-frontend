5e7fa3ec78bc952559bdbcc857e6e6b4be63586f
Abdu1RH <abdul.rh2403@gmail.com>
2022-11-22 11:21:33 +0100

    FILTER

--- main.js
+++ main.js
@@ -5,6 +5,7 @@
 
 
 
+
     });
 
 function renderSpots(spots) {
@@ -29,39 +30,41 @@ function renderSpots(spots) {
     // create element
 
 }
+ /*
+ fetch("https://igspots.onrender.com/users")
+     .then(response => response.json())
+     .then(function (users) {
+         renderUsers(users);
 
-fetch("https://igspots.onrender.com/users")
-    .then(response => response.json())
-    .then(function (users) {
-        renderUsers(users);
 
 
+     });
 
-    });
+ function renderUsers(users) {
+     console.log(users);
+     const ul = document.querySelector('ul#users');
+     console.log(ul);
+     for (let i = 0; i < users.length; i++) {
+         const user = users[i];
+         console.log(user);
+         const li = document.createElement('li');
+         li.innerHTML = `
+             <p class="user_id">${user.user_id}</p>
+             <p class="user_name">${user.user_name}</p>
+             <p class="email">${user.email}</p>
+             <p class="user_password">${user.user_password}</p>
+         `;
+         ul.appendChild(li);
+     }
 
-function renderUsers(users) {
-    console.log(users);
-    const ul = document.querySelector('ul#users');
-    console.log(ul);
-    for (let i = 0; i < users.length; i++) {
-        const user = users[i];
-        console.log(user);
-        const li = document.createElement('li');
-        li.innerHTML = `
-            <p class="user_id">${user.user_id}</p>
-            <p class="user_name">${user.user_name}</p>
-            <p class="email">${user.email}</p>
-            <p class="user_password">${user.user_password}</p> 
-        `;
-        ul.appendChild(li);
-    }
-
-}
+ }
+  */
 
 fetch("https://igspots.onrender.com/images")
     .then(response => response.json())
     .then(function (images) {
         renderImages(images);
+        filterSeasons(images);
 
 
 
@@ -70,23 +73,49 @@ fetch("https://igspots.onrender.com/images")
 function renderImages(images) {
     console.log(images);
     const ul = document.querySelector('ul#images');
+    ul.innerHTML=""
     console.log(ul);
     for (let i = 0; i < images.length; i++) {
         const image = images[i];
         console.log(image);
         const li = document.createElement('li');
         li.innerHTML = `
-            <p class="angle">${image.angle}</p>
+            <p class="img_id">${image.img_id}</p>
             <p class="season">${image.season}</p>
+            <p class="angle">${image.angle}</p>
             <p class="tag">${image.tag}</p>
-            <p class="img_id">${image.img_id}</p> 
         `;
         ul.appendChild(li);
     }
 
 }
 
+ function filterSeasons(images) {
+     console.log(images);
+     const button = document.querySelector('button.filter');
+     console.log(button);
+     button.addEventListener("click", function () {
+         console.log("button clicked");
+         const inputElement = document.querySelector('input');
+         console.log(inputElement);
+         const filteredSeason = inputElement.value;
+         console.log(filteredSeason);
+         const seasonFiltered = [];
+         console.log(seasonFiltered)
+         for (let i = 0; i < images.length; i++) {
+             const image = images[i];
+             console.log(image.season);
+             console.log(filteredSeason);
+             console.log(image.season === filteredSeason);
+             if ((image.season) === (filteredSeason)) {
+                 seasonFiltered.push(image);
+             }
+         }
 
+         renderImages(seasonFiltered)
+     })
+
+ }
 
 
 
