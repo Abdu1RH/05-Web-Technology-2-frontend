9e9917ba3e2e1d8afbc088479a2557ba985034f2
Abdu1RH <abdul.rh2403@gmail.com>
2022-11-25 13:19:10 +0100

    FINAL COMMIT AND PUSH

--- main.js
+++ main.js
@@ -8,172 +8,69 @@
 
 function renderSpots(spots) {
     console.log(spots);
-    const ul = document.querySelector('#spots');
-    ul.innerHTML = ""
-    console.log(ul);
+
+    const spotDiv = document.querySelector('#spots');
+    spotDiv.innerHTML = ""
+    console.log(spotDiv);
+
     for (let i = 0; i < spots.length; i++) {
+
         const spot = spots[i];
         console.log(spot);
+
         const li = document.createElement('div');
 
-        /*
-        <p class="spot_id">${spot.spot_id}</p>
-            <p class="location_name">${spot.location_name}</p>
-            <p class="address">${spot.address}</p>
-            <p class="longtitude">${spot.longitude}</p>
-            <p class="latitude">${spot.latitude}</p>
-         */
-        li.innerHTML = `
-            
+        li.innerHTML = `  
       
-<div class="image-container">
-    <img src="${spot.img_link}" alt=""/>
-        <span>
-                <p> ${spot.spot_id}</p>
-                <h1>${spot.location_name}</h1>
-                <p>
-                ${spot.address}
-                <br>
-                ${spot.season}
-               </p>
-        </span>    
-    </div>   
-</div>
+        <div class="image-container">
+            <img src="${spot.img_link}" alt=""/>
+                <span>
+                     <p> ${spot.spot_id}</p>
+                     <h1>${spot.location_name}</h1>
+                     <p> ${spot.address}
+                        <br>
+                         ${spot.season}</p>
+                </span>    
+        </div>   
+
         `;
-        ul.appendChild(li);
+        spotDiv.appendChild(li);
     }
 }
+
+
+
  function filterSeasons(spots) {
      console.log(spots);
+
      const button = document.querySelector('button.filter');
      console.log(button);
+
      button.addEventListener("click", function () {
          console.log("button clicked");
+
          const inputElement = document.querySelector('input');
          console.log(inputElement);
+
          const filteredSeason = inputElement.value;
          console.log(filteredSeason);
+
          const seasonFiltered = [];
-         console.log(seasonFiltered)
+         console.log(seasonFiltered);
+
          for (let i = 0; i < spots.length; i++) {
              const spot = spots[i];
              console.log(spot.season);
              console.log(filteredSeason);
              console.log(spot.season === filteredSeason);
+
              if ((spot.season) === (filteredSeason)) {
                  seasonFiltered.push(spot);
              }
          }
-         renderSpots(seasonFiltered)
-     })
- }
-
-
-
-    // lave et loop
-    // create element
-
-
- /*
- fetch("https://igspots.onrender.com/user_spot")
-     .then(response => response.json())
-     .then(function (users) {
-         renderUsers(users);
-
-
-
+         renderSpots(seasonFiltered);
      });
-
- function renderUsers(users) {
-     console.log(users);
-     const ul = document.querySelector('ul#users');
-     console.log(ul);
-     for (let i = 0; i < users.length; i++) {
-         const user = users[i];
-         console.log(user);
-         const li = document.createElement('li');
-         li.innerHTML = `
-
-    <div class="image-container">
-        <img src="${spot.img_link}" alt=""/>
-        <span>
-             <p class="user_id">${user.user_id}</p>
-             <p class="user_name">${user.user_name}</p>
-             <p class="email">${user.email}</p>
-             <p class="user_password">${user.user_password}</p>
-         </span>
-
-    </div>
-
-         `;
-         ul.appendChild(li);
-     }
-
  }
-  */
-
-
-/*
-fetch("https://igspots.onrender.com/images")
-    .then(response => response.json())
-    .then(function (images) {
-        renderImages(images);
-        filterSeasons(images);
-
-
-
-    });
-
-function renderImages(images) {
-    console.log(images);
-    const ul = document.querySelector('ul#images');
-    ul.innerHTML=""
-    console.log(ul);
-    for (let i = 0; i < images.length; i++) {
-        const image = images[i];
-        console.log(image);
-        const li = document.createElement('li');
-        li.innerHTML = `
-            <p class="img_id">${image.img_id}</p>
-            <p class="img_link">${image.img_link}</p>
-            `;
-        ul.appendChild(li);
-    }
-
-}
- */
-
- /*
- function filterSeasons(images) {
-     console.log(images);
-     const button = document.querySelector('button.filter');
-     console.log(button);
-     button.addEventListener("click", function () {
-         console.log("button clicked");
-         const inputElement = document.querySelector('input');
-         console.log(inputElement);
-         const filteredSeason = inputElement.value;
-         console.log(filteredSeason);
-         const seasonFiltered = [];
-         console.log(seasonFiltered)
-         for (let i = 0; i < images.length; i++) {
-             const image = images[i];
-             console.log(image.season);
-             console.log(filteredSeason);
-             console.log(image.season === filteredSeason);
-             if ((image.season) === (filteredSeason)) {
-                 seasonFiltered.push(image);
-             }
-         }
-
-         renderImages(seasonFiltered)
-     })
-
- }
-
-  */
-
-
 
  fetch("https://igspots.onrender.com/user_spot")
      .then(response => response.json())
@@ -183,18 +80,22 @@ function renderImages(images) {
 
  function renderUsers(userSpots) {
      console.log(userSpots);
-     const ul = document.querySelector('ul#user_spots');
-     ul.innerHTML = ""
-     console.log(ul);
+
+     const spotDiv = document.querySelector('ul#user_spots');
+     spotDiv.innerHTML = ""
+     console.log(spotDiv);
+
      for (let i = 0; i < userSpots.length; i++) {
+
          const user = userSpots[i];
          console.log(user);
+
          const li = document.createElement('li');
          li.innerHTML = `
 
-<div class="image-container-users">
-    <img src="${user.img_link}" alt=""/>
-        <span>
+        <div class="image-container-users">
+          <img src="${user.img_link}" alt=""/>
+            <span>
                 <p> ${user.spot_id}</p>
                 <h1>${user.location_name}</h1>
                 <p>
@@ -202,16 +103,10 @@ function renderImages(images) {
                 <br>
                 ${user.user_mail}
                </p>
-        </span>    
-    </div>   
-</div>
-         
+            </span>    
+        </div>   
+       
         `;
-         ul.appendChild(li);
+         spotDiv.appendChild(li);
      }
-
-
 }
-
-
-
